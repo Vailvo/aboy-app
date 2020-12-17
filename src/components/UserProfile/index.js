7b0f62b390e672de1c6aa228488477c7fc6ceed2
Shazé Williams <shazewilliams@live.com>
2020-12-16 19:45:30 -0800

    Debug
    Need to debug multiple TypeErrors

---
+++ src/components/UserProfile/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './styles.scss';
+
+
+const UserProfile = props => {
+  const { currentUser } = props;
+  const { displayName } = currentUser;
+
+  return (
+    <div className="userProfile">
+      <ul>
+        <li>
+          <div className="img">
+            {/* <img src={} /> */}
+          </div>
+        </li>
+        <li>
+          <span className="displayName">
+            {displayName && displayName}
+          </span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default UserProfile;
